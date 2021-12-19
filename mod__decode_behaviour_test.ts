5540efe35a6de48b4819fe27260439fb28552248
Hal Blackburn <hwtb2@cam.ac.uk>
2021-12-19 13:53:28 +0000

    wip: test: add tests for spec decoding behaviour

--- mod__decode_behaviour_test.ts
+++ mod__decode_behaviour_test.ts
@@ -1,16 +1,16 @@
 /** Tests for decoder behaviour of malformed streams. */
 
-import {
-  assert,
-  assertEquals,
-  AssertionError,
-  readableStreamFromIterable,
-} from "./dev_deps.ts";
+import { assertEquals, readableStreamFromIterable } from "./dev_deps.ts";
 import {
   JSON_SEQ_END as NL,
   JSON_SEQ_START as RS,
   JsonSequenceDecoderStream,
 } from "./mod.ts";
+import {
+  recordStreamEvents,
+  SinkAbort,
+  SinkEvent,
+} from "./_testing/stream_recorder.ts";
 
 type Json = null | boolean | number | string | { [key: string]: Json } | Json[];
 
@@ -29,7 +29,7 @@ const cases: ReadonlyArray<{
    * Values emitted by the strict decoder. An error is expected to be emitted
    * after the final item.
    */
-  strictDecoderResult: ReadonlyArray<Json>;
+  strictDecoderResult: ReadonlyArray<Json | typeof Error>;
 }> = [
   // Section 2 and 2.3 of the RFC encourage decoder's to recover when truncated
   // elements are encountered by skipping to the next RS and ignoring the
@@ -40,71 +40,82 @@ const cases: ReadonlyArray<{
     name: "truncated initial chunk containing valid JSON",
     streamContent: `{"a":0}${NL}${RS}{"a":1}${NL}`,
     normalDecoderResult: [{ a: 1 }],
-    strictDecoderResult: [],
+    strictDecoderResult: [Error],
   },
   {
     name: "truncated initial chunk containing junk",
     streamContent: `asdf${NL}${RS}{"a":1}${NL}`,
     normalDecoderResult: [{ a: 1 }],
-    strictDecoderResult: [],
+    strictDecoderResult: [Error],
   },
   {
     name: "truncated intermediate chunk containing valid JSON",
     // missing NL at end of a:1
     streamContent: `${RS}{"a":0}${NL}${RS}{"a":1}${RS}{"a":2}${NL}`,
     normalDecoderResult: [{ a: 0 }, { a: 1 }, { a: 2 }],
-    strictDecoderResult: [{ a: 0 }],
+    strictDecoderResult: [{ a: 0 }, Error],
   },
 ];
 
 for (const { name, streamContent, normalDecoderResult } of cases) {
+  // Deno.test(`FIXME: rm [decode/non-strict]: ${name}`, async () => {
+  //   let stream: ReadableStream<unknown> | undefined = undefined;
+  //   let reader: ReadableStreamReader<unknown> | undefined = undefined;
+  //   try {
+  //     stream = readableStreamFromIterable([streamContent])
+  //       .pipeThrough(new TextEncoderStream())
+  //       .pipeThrough(new JsonSequenceDecoderStream({ strict: false }));
+  //     reader = stream.getReader();
+  //     for (
+  //       const { json: expectedJson, i } of normalDecoderResult.map((
+  //         json,
+  //         i,
+  //       ) => ({
+  //         json,
+  //         i,
+  //       }))
+  //     ) {
+  //       const { done, value: actualJson } = await reader.read();
+  //       assert(
+  //         done === false,
+  //         `premature end of stream: i=${i}, expectedJson=${
+  //           Deno.inspect(expectedJson)
+  //         }`,
+  //       );
+  //       assertEquals(
+  //         actualJson,
+  //         expectedJson,
+  //         `unexpected decoder output: i=${i}, expectedJson=${
+  //           Deno.inspect(expectedJson)
+  //         }, actualJson=${Deno.inspect(actualJson)}`,
+  //       );
+  //     }
+  //     const { done, value } = await reader.read();
+  //     assert(
+  //       done === true,
+  //       `stream produced value when end-of-stream expected: value=${
+  //         Deno.inspect(value)
+  //       }`,
+  //     );
+  //   } catch (e) {
+  //     if (e instanceof AssertionError) {
+  //       throw e;
+  //     }
+  //     throw new AssertionError(`Unexpected error: ${Deno.inspect(e)}`);
+  //   } finally {
+  //     // await reader ? reader?.cancel() : stream?.cancel();
+  //   }
+  // });
+
   Deno.test(`[decode/non-strict]: ${name}`, async () => {
-    let stream: ReadableStream<unknown> | undefined = undefined;
-    let reader: ReadableStreamReader<unknown> | undefined = undefined;
-    try {
-      stream = readableStreamFromIterable([streamContent])
-        .pipeThrough(new TextEncoderStream())
-        .pipeThrough(new JsonSequenceDecoderStream({ strict: false }));
-      reader = stream.getReader();
-      for (
-        const { json: expectedJson, i } of normalDecoderResult.map((
-          json,
-          i,
-        ) => ({
-          json,
-          i,
-        }))
-      ) {
-        const { done, value: actualJson } = await reader.read();
-        assert(
-          done === false,
-          `premature end of stream: i=${i}, expectedJson=${
-            Deno.inspect(expectedJson)
-          }`,
-        );
-        assertEquals(
-          actualJson,
-          expectedJson,
-          `unexpected decoder output: i=${i}, expectedJson=${
-            Deno.inspect(expectedJson)
-          }, actualJson=${Deno.inspect(actualJson)}`,
-        );
-      }
-      const { done, value } = await reader.read();
-      assert(
-        done === true,
-        `stream produced value when end-of-stream expected: value=${
-          Deno.inspect(value)
-        }`,
-      );
-    } catch (e) {
-      if (e instanceof AssertionError) {
-        throw e;
-      }
-      throw new AssertionError(`Unexpected error: ${Deno.inspect(e)}`);
-    } finally {
-      // await reader ? reader?.cancel() : stream?.cancel();
-    }
+    const stream = readableStreamFromIterable([streamContent])
+      .pipeThrough(new TextEncoderStream())
+      .pipeThrough(new JsonSequenceDecoderStream({ strict: false }));
+
+    assertEquals(
+      await recordStreamEvents(stream, "write", "abort"),
+      normalDecoderResult.map((json) => ({ type: "write", chunk: json })),
+    );
   });
 }
 
@@ -116,6 +127,7 @@ for (const { name, streamContent, normalDecoderResult } of cases) {
 
 // if weve leaked the text decoder it coul be throwing the pronise error when the leak is found
 
+/*
 for (const { name, streamContent, strictDecoderResult } of cases) {
   Deno.test(`[decode/strict]: ${name}`, async () => {
     let stream: ReadableStream<unknown> | undefined = undefined;
@@ -174,4 +186,61 @@ for (const { name, streamContent, strictDecoderResult } of cases) {
       }
     }
   });
+}
+*/
+
+for (const { name, streamContent, strictDecoderResult } of cases) {
+  Deno.test(`[decode/strict]: ${name}`, async () => {
+    const shouldError = strictDecoderResult.some((x) => x === Error);
+    let srcCancelled = false;
+    // const stream = readableStreamFromIterable([streamContent])
+    const stream = new ReadableStream({
+      cancel() {
+        srcCancelled = true;
+      },
+      start(controller) {
+        controller.enqueue(streamContent);
+      },
+    })
+      .pipeThrough(new TextEncoderStream())
+      .pipeThrough(new JsonSequenceDecoderStream({ strict: false }));
+
+    const events = await recordStreamEvents(stream, "write", "abort");
+    assertEquals(srcCancelled, shouldError);
+    assertRecordedEventsMatch(events, {
+      chunks: strictDecoderResult.filter((x): x is Json => x !== Error),
+      abort: shouldError,
+    });
+  });
+}
+
+type AssertRecordedEventsMatchOptions<T> = {
+  chunks?: ReadonlyArray<T>;
+  abort?: boolean;
+};
+
+function assertRecordedEventsMatch<T>(
+  events: ReadonlyArray<SinkEvent<T>>,
+  options?: AssertRecordedEventsMatchOptions<T>,
+) {
+  const { chunks, abort } = options ?? {};
+  const expected: NormalisedSinkEvent<T>[] = [
+    ...(events.at(0)?.type === "start" ? [{ type: "start" } as const] : []),
+    ...(chunks ?? []).map((chunk) => ({ type: "write", chunk } as const)),
+    abort ? { type: "abort", reason: "*any*" } : { type: "close" },
+  ];
+  assertEquals(normaliseEventsForEquality(events), expected);
+}
+
+// Could allow specifying a key function for reason if actual matching is
+// needed. E.g. match a regex by using the match's groups as the equality key.
+
+type AnySinkAbort = Omit<SinkAbort, "reason"> & { reason: "*any*" };
+type NormalisedSinkEvent<T> = Exclude<SinkEvent<T>, SinkAbort> | AnySinkAbort;
+function normaliseEventsForEquality<T>(
+  events: ReadonlyArray<SinkEvent<T>>,
+): NormalisedSinkEvent<T>[] {
+  return events.map((event) =>
+    event.type === "abort" ? { type: "abort", reason: "*any*" } : event
+  );
 }
